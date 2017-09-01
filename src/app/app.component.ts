5995de6c932099c99a14a622a6041b91cde98862
poulpan <pan.poulidakis@gmail.com>
2017-09-01 14:45:09 +0300

    AngularCLI Carousel

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  label_a = "Tazmanian Devil";
+  label_b = "Bugs Bunny";
+  label_c = "Marvin the Martian";
+}
